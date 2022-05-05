b46db1368d58f713243011a66309dbd80d71a3ea
OnistDerFalke <bazaanfil@gmail.com>
2022-05-05 14:01:53 +0200

    Token based app-server authentication

--- client/api_gate.py
+++ client/api_gate.py
@@ -1,31 +1,25 @@
 import requests
+import os
+
+from token_requests import UserTokenizedRequester
 
 
 # get list of the users from API
 def get_users_list():
-    try:
-        response = requests.get("http://127.0.0.1:8080/api/users")
-        users = response.json()
-        return users
-    except:
-        print("Could not connect to local server.")
+    token_path = os.path.join('token', 'token.json')
+    utr = UserTokenizedRequester(token_path, 'http://127.0.0.1:8080/')
+    return utr.get_users_list()
 
 
 # registering user in API
 def reg_user_in_api(username, port):
-    response = requests.post('http://127.0.0.1:8080/api/users', json={
-        "name": username,
-        "port": port
-    })
-    print(f"User register with status code: {response.status_code}")
+    token_path = os.path.join('token', 'token.json')
+    utr = UserTokenizedRequester(token_path, 'http://127.0.0.1:8080/')
+    utr.reg_user(username, port)
 
 
 # unregistering user in API
 def unreg_user_in_api(username, port):
-    users = get_users_list()
-    for user in users:
-        if user["name"] == username and user["port"] == port:
-            response = requests.delete('http://127.0.0.1:8080/api/users/'+str(user["id"]))
-            print(f"User successfully unregistered with status: {response.status_code}")
-            return
-    print("Could not unregister user with username and port given.")
+    token_path = os.path.join('token', 'token.json')
+    utr = UserTokenizedRequester(token_path, 'http://127.0.0.1:8080/')
+    utr.unreg_user(username, port)
