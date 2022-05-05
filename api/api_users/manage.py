1584d4b4c1a240561c440423cd516099168d9d95
TheWinczi <mwinczewski@tutanota.com>
2022-05-05 12:18:35 +0200

    UserTokenizedRequest class was added to make sending tokenized requests easier.

--- api/api_users/manage.py
+++ api/api_users/manage.py
@@ -1,56 +1,6 @@
-import json
-import os
 import sys
-from base64 import b64encode
-
-import requests
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad
 
 from src.modules.commands_parser import ManageCommandParser
-
-
-# TODO - export UserTokenRequester to other file with all needed functions
-
-
-class UserTokenRequester(object):
-    def __init__(self, token_file_path: str):
-        self.token_file_path = token_file_path
-        self.validate_state()
-
-    def validate_state(self):
-        if not os.path.exists(self.token_file_path):
-            raise FileNotFoundError(f'Token file was not found in {self.token_file_path}')
-
-    def _get_token_fields(self):
-        with open(self.token_file_path, 'r') as file:
-            return json.loads(file.read())
-
-    def _prepare_token_request_header(self):
-        token = self._get_token_fields()
-
-        login = token.get('login', '')
-        secret = token.get('secret', '')
-        key = token.get('key', '').encode('utf-8')
-
-        cipher = AES.new(key, AES.MODE_CBC)
-        secret_bin = pad(secret.encode('utf-8'), AES.block_size)
-
-        secret_bytes = cipher.encrypt(secret_bin)
-
-        iv = b64encode(cipher.iv).decode('utf-8')
-        secret = b64encode(secret_bytes).decode('utf-8')
-
-        return json.dumps({
-            'login': login,
-            'secret': secret,
-            'iv': iv
-        })
-
-    def get_all_users(self):
-        return requests.get('http://127.0.0.1:8080/api/users', headers={
-            'token': self._prepare_token_request_header(),
-        })
 
 
 def main(argv: list[str]):
